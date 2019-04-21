569b8d82dbe0103f19d13b2408f335296834a608
MrGio7 <lashagiorgi1233@gmail.com>
2019-04-21 23:53:21 +0400

    updated tabs component

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,10 +18,15 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.state.tabs = tab
   };
 
   filterCards = () => {
@@ -37,18 +42,27 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+   if(this.state.selected === 'all'){
+     return this.state.cards
+   }
+    return this.state.selected;
   };
 
   render() {
+    console.log()
     return (
       <div className="content-container">
         {/* 
           Add 2 props to the Tabs component, 
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
-        */}
-        <Tabs tabs={this.state.tabs} />
+        */
+       }
+        <Tabs 
+          selectedTab={this.state.selected}
+          selectTabHandler={this.changeSelected}
+          tabs={this.state.tabs}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
