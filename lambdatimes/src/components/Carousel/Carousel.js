9a34646986ce26ab2ff2c7060cb905bc51df4c38
MrGio7 <lashagiorgi1233@gmail.com>
2019-04-22 22:29:25 +0400

    Not working carousel model

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,34 +1,98 @@
 import React, { Component } from 'react';
 // Complete this Carousel 
-import { UncontrolledCarousel } from 'reactstrap';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 
 const items = [
   {
     src: '../../assets/carousel/mountains.jpeg',
     altText: 'Slide 1',
-    caption: 'Slide 1',
-    header: 'Slide 1 Header'
+    caption: 'Slide 1'
   },
   {
-    src: '../../assets/carousel/computer.jpeg',
+    src: '../../assets/carousel/mountains.jpeg',
     altText: 'Slide 2',
-    caption: 'Slide 2',
-    header: 'Slide 2 Header'
+    caption: 'Slide 2'
   },
   {
-    src: '../../assets/carousel/trees.jpeg',
+    src: '../../assets/carousel/mountains.jpeg',
     altText: 'Slide 3',
-    caption: 'Slide 3',
-    header: 'Slide 3 Header'
-  },
-  {
-    src: '../../assets/carousel/turntable.jpeg',
-    altText: 'Slide 4',
-    caption: 'Slide 4',
-    header: 'Slide 4 Header'
+    caption: 'Slide 3'
   }
 ];
 
-const Example = () => <UncontrolledCarousel items={items} />;
+class Example extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.altText}
+        >
+          <img src={item.src} alt={item.altText} />
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
+    );
+  }
+}
+
 
 export default Example;
+
