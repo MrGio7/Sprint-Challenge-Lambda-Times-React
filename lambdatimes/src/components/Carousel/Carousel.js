88e526c2b5b6146dc7d8a16f54ecd7695cad947a
MrGio7 <lashagiorgi1233@gmail.com>
2019-04-22 22:20:41 +0400

    added carusel from reactstrap

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,34 @@
 import React, { Component } from 'react';
-import { carouselData } from '../../data'
 // Complete this Carousel 
-export default class Carousel extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
+import { UncontrolledCarousel } from 'reactstrap';
 
-    }
+const items = [
+  {
+    src: '../../assets/carousel/mountains.jpeg',
+    altText: 'Slide 1',
+    caption: 'Slide 1',
+    header: 'Slide 1 Header'
+  },
+  {
+    src: '../../assets/carousel/computer.jpeg',
+    altText: 'Slide 2',
+    caption: 'Slide 2',
+    header: 'Slide 2 Header'
+  },
+  {
+    src: '../../assets/carousel/trees.jpeg',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    header: 'Slide 3 Header'
+  },
+  {
+    src: '../../assets/carousel/turntable.jpeg',
+    altText: 'Slide 4',
+    caption: 'Slide 4',
+    header: 'Slide 4 Header'
   }
-  componentDidMount(){
+];
 
-  }
+const Example = () => <UncontrolledCarousel items={items} />;
 
-  leftClick = () => {
-
-  }
-
-  rightClick = () => {
-
-  }
-
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
-  render(){
-    return (
-      <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
-      </div>
-    )
-  }
-}
+export default Example;
